e7151d44740b9b85756d13c798dd8cabba5ac3ed
Lup38 <105895723+Lup38@users.noreply.github.com>
2025-07-31 19:52:31 -0300

    menu basico

--- Assets/Scripts/Mainmenu/MainMenu.cs
+++ Assets/Scripts/Mainmenu/MainMenu.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+  
+
+
+    public void Play()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Exit()
     {
-        
+        Application.Quit();
     }
+
+  
 }
