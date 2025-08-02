fc3805c2a105e3e3e62b4ca0ab29a71f221aa909
ViniciusVsV <viniciusvsv193@gmail.com>
2025-08-02 18:41:13 -0300

    Controlador do menu final

---
+++ Assets/Scripts/FinallMenuController.cs
@@ -0,0 +1,12 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinallMenuController : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+            SceneManager.LoadScene("MainMenu");
+    }
+}
